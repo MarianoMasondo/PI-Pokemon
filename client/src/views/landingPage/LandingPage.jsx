f64a0866960ab1744891e67c599d427a8748cd6e
Mariano Masondo <nanomason@gmail.com>
2026-05-16 13:45:39 -0300

    Fixes 2

--- client/src/views/landingPage/LandingPage.jsx
+++ client/src/views/landingPage/LandingPage.jsx
@@ -1,30 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
   return (
-    <div className={styles.landingContainer}>
-      <h1>
-        <a href="https://fontmeme.com/es/doblar-imagenes/">
-          <img
-            src="https://fontmeme.com/permalink/230731/f83fdfdb2482b4c1f7620adc769b5f00.png"
-            alt="doblar-imagenes"
-            border="0"
-          />
-        </a>
-      </h1>
-      <Link to="/home">
-        <div className={styles.pokeball}>
-          <button
-            className={`${styles.pokeball__button} centered-text`}
-            type="submit"
-          >
+    <main className={styles.landingContainer}>
+      <div className={styles.pokeballContainer}>
+        <NavLink to="/home" className={styles.pokeball}>
+          <div className={styles.pokeballButton}>
             <span>Enter</span>
-          </button>
-        </div>
-      </Link>
-    </div>
+          </div>
+        </NavLink>
+      </div>
+    </main>
   );
 };
 
