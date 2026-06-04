9acb43add3a76e3ce8791b85536d2bbd4ab3ff5f
Mariano Masondo <nanomason@gmail.com>
2026-06-04 11:35:28 -0300

    Vue config

---
+++ client-vue/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/home",
+            name: "home",
+            component: HomeView
+        }
+    ]
+})
+
+export default router
---
+++ client-vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <main>
+        <h1>Home Vue Funcionando</h1>
+        <p>Más adelante vamos a mostrar acá las tarjetas de Pokemon</p>
+    </main>
+</template>
+
+<style scoped></style>
